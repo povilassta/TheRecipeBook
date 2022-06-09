9e92e687c7fd75d90382f5e73d7c5c85ca1c7d97
Povilas Stasys <povilas1999@gmail.com>
2022-06-09 11:12:09 +0300

    category tag component

---
+++ src/app/recipe-catalog/recipe-catalog/category-tag/category-tag.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-tag',
+  templateUrl: './category-tag.component.html',
+  styleUrls: ['./category-tag.component.sass']
+})
+export class CategoryTagComponent implements OnInit {
+
+  constructor() { }
+
+  @Input()
+  public message!: string;
+
+  ngOnInit(): void {
+  }
+
+}
