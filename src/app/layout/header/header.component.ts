7e79547301fef39f5c9bf655c0c8b976f959d8b9
Povilas Stasys <povilas1999@gmail.com>
2022-06-13 14:01:37 +0300

    header

---
+++ src/app/layout/header/header.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { SharedService } from 'src/app/services/shared.service';
+
+@UntilDestroy()
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.sass'],
+})
+export class HeaderComponent implements OnInit {
+  constructor(private sharedService: SharedService) {
+    sharedService.updateHeaderCalled$
+      .pipe(untilDestroyed(this))
+      .subscribe(
+        () => (this.currentUser = localStorage.getItem('email') || '')
+      );
+  }
+
+  public currentUser: string = localStorage.getItem('email') || '';
+
+  public logout(): void {
+    localStorage.removeItem('email');
+    this.currentUser = '';
+  }
+
+  ngOnInit(): void {}
+}
