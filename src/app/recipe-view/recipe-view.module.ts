d837ebb8310a345933b4c2641d6d64107b676ab5
Povilas Stasys <povilas1999@gmail.com>
2022-06-13 12:05:05 +0300

    fix image shrinking with screen, add signin to comment message

--- src/app/recipe-view/recipe-view.module.ts
+++ src/app/recipe-view/recipe-view.module.ts
@@ -5,9 +5,16 @@ import { RecipeCatalogModule } from '../recipe-catalog/recipe-catalog.module';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { CommentCardComponent } from './comment-card/comment-card.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [RecipeComponent, CommentCardComponent],
-  imports: [CommonModule, RecipeCatalogModule, MatIconModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    RecipeCatalogModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterModule,
+  ],
 })
 export class RecipeViewModule {}
