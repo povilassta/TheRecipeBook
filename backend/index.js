7047b8cb064ef791ed4d3c08e4f3991f6886ed3f
povilassta <povilas1999@gmail.com>
2022-06-14 13:08:12 +0300

    user crud

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import recipeRouter from "./routes/recipe.js";
+import userRouter from "./routes/user.js";
 
 const app = express();
 const port = 3000;
 
 // Routes
 app.use("/recipes", recipeRouter);
+app.use("/users", userRouter);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
