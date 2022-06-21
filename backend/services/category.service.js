2a1e9b3f23c742cbcf5c1b55828b09b25536b6f3
povilassta <povilas1999@gmail.com>
2022-06-21 08:56:53 +0300

    remove useless code

--- backend/services/category.service.js
+++ backend/services/category.service.js
@@ -4,51 +4,6 @@ const CategoryService = {
   getForRecipes: async () => {
     try {
       const categories = await Category.find({});
-      //   const categories = await Recipe.aggregate([
-      //     {
-      //       $unwind: "$categories",
-      //     },
-      //     {
-      //       $lookup: {
-      //         from: "categories",
-      //         as: "categoriesFlat",
-      //         let: {
-      //           categoryObjectId: {
-      //             $toObjectId: "$categories",
-      //           },
-      //         },
-      //         pipeline: [
-      //           {
-      //             $match: {
-      //               $expr: {
-      //                 $eq: ["$$categoryObjectId", "$_id"],
-      //               },
-      //             },
-      //           },
-      //         ],
-      //       },
-      //     },
-      //     {
-      //       $group: {
-      //         _id: null,
-      //         categories: {
-      //           $push: {
-      //             _id: {
-      //               $first: "$categoriesFlat._id",
-      //             },
-      //             name: {
-      //               $first: "$categoriesFlat.name",
-      //             },
-      //           },
-      //         },
-      //       },
-      //     },
-      //     {
-      //       $project: {
-      //         _id: 0,
-      //       },
-      //     },
-      //   ]);
       return categories;
     } catch (errors) {
       throw errors;
