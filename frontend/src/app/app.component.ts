8589230090bf11f616b52671be6020687732dd97
povilassta <povilas1999@gmail.com>
2022-07-01 15:40:06 +0300

    add sidenav

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { ComponentCommunicationService } from './services/componentCommunication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
+  constructor(
+    private componentCommunicationService: ComponentCommunicationService
+  ) {
+    this.componentCommunicationService.updateUserCalled$.subscribe(
+      () => (this.currentUser = localStorage.getItem('username') || '')
+    );
+  }
   title = 'recipe-book';
+
+  @ViewChild('sidenav')
+  private sidenav: MatSidenav | undefined;
+  public currentUser: string = localStorage.getItem('username') || '';
+
+  @HostListener('window:resize', ['$event'])
+  public onResize(event: { target: { innerWidth: number } }): void {
+    if (event.target.innerWidth > 700) {
+      this.sidenav?.close();
+    }
+  }
 }
