945a2ba57beee639d291d51329b1f29dbf11d86f
povilassta <povilas1999@gmail.com>
2022-07-18 10:09:24 +0300

    fix inconsistencies with snapshots and add login

--- frontend/cypress/e2e/anonymous.cy.ts
+++ frontend/cypress/e2e/anonymous.cy.ts
@@ -1,26 +1,39 @@
 describe('Anonymous tests', () => {
-  beforeEach(() => {});
+  beforeEach(() => {
+    // Set aliases for request
+    cy.intercept('post', '/api/recipes').as('getRecipes');
+    cy.intercept('get', '/api/recipes/**').as('getRecipe');
+    cy.intercept('get', '/api/recipes/**/comments').as('getComments');
+  });
 
   it('Visits the initial project page', () => {
     cy.visit('/');
     cy.contains('Variety of silly Recipes');
     cy.get('div[class=card]').should('exist');
-    cy.percySnapshot();
+    cy.wait('@getRecipes');
+    cy.percySnapshot("Unauthenticated user's homepage"); // SNAPSHOT
   });
 
-  it('Visits the login page', () => {
+  it('Visits the login page and logs in', () => {
     cy.visit('/login');
     cy.contains('Sign in').should('exist');
-    cy.percySnapshot();
+    cy.percySnapshot('Sign in page'); // SNAPSHOT
+
+    cy.get('[type=email]').type('test@test.com');
+    cy.get('[type=password]').type('test');
+    cy.get('button').contains('Sign in').click();
+    cy.url().should('include', '/recipes');
   });
 
   it('Changes language to Lithuanian and back to english', () => {
     cy.visit('/');
+    cy.wait('@getRecipes');
     cy.contains('Variety of silly Recipes');
     cy.get('[aria-label="Language menu trigger"]').click();
     cy.get('button').contains('LT').click();
-    cy.percySnapshot();
     cy.contains('Smagių receptų įvairovė').should('exist');
+    cy.wait(1000); // WAIT FOR SNAPSHOT
+    cy.percySnapshot('Lithuanian homepage'); // SNAPSHOT
     cy.get('[aria-label="Language menu trigger"]').click();
     cy.get('button').contains('EN').click();
     cy.contains('Variety of silly Recipes').should('exist');
@@ -32,14 +45,14 @@ describe('Anonymous tests', () => {
     });
     cy.visit('/');
     cy.contains('Something went wrong').should('exist');
-    cy.percySnapshot();
+    cy.percySnapshot('Something went wrong homepage'); // SNAPSHOT
   });
 
   it('Redirects to 404', () => {
     cy.visit('/someroutethatdefinitelydoesnotexist');
     cy.contains(404).should('exist');
     cy.url().should('include', '/404');
-    cy.percySnapshot();
+    cy.percySnapshot('404 page'); // SNAPSHOT
     cy.contains('Go back home').click();
     cy.url().should('include', '/recipes');
   });
@@ -50,9 +63,11 @@ describe('Anonymous tests', () => {
       .contains('Share your own recipe!')
       .should('not.exist');
     cy.get('.card').first().click();
+    cy.wait('@getRecipe');
+    cy.wait('@getComments');
     cy.get('[aria-label="Recipe options menu button"]').should('not.exist');
     cy.contains('Submit').should('not.exist');
     cy.contains('You must Sign in in order to comment!');
-    cy.percySnapshot();
+    cy.percySnapshot('Recipe page (Unauth POV)'); // SNAPSHOT
   });
 });
