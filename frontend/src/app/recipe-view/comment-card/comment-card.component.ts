0ed0b5a0a3ff4dfdefa81463b3b8e5efecfb49e6
povilassta <povilas1999@gmail.com>
2022-07-18 15:49:28 +0300

    date localization

--- frontend/src/app/recipe-view/comment-card/comment-card.component.ts
+++ frontend/src/app/recipe-view/comment-card/comment-card.component.ts
@@ -1,16 +1,27 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 import { Comment } from 'src/app/models/comment.model';
 
+@UntilDestroy()
 @Component({
   selector: 'app-comment-card',
   templateUrl: './comment-card.component.html',
   styleUrls: ['./comment-card.component.sass'],
 })
 export class CommentCardComponent implements OnInit {
-  constructor() {}
+  constructor(private translate: TranslateService) {
+    this.translate.onLangChange
+      .pipe(untilDestroyed(this))
+      .subscribe((langChangeEvent: LangChangeEvent) => {
+        this.locale = langChangeEvent.lang;
+      });
+  }
 
   @Input()
   public comment: Comment | undefined;
+
+  public locale = localStorage.getItem('language') || 'en';
 
   public ngOnInit(): void {}
 }
