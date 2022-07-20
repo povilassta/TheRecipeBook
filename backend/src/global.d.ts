65d05b5cdadc601dce723d9676262c145b472757
povilassta <povilas1999@gmail.com>
2022-07-20 14:37:40 +0300

    extend interfaces

---
+++ backend/src/global.d.ts
@@ -0,0 +1,14 @@
+import "express";
+
+declare global {
+  namespace Express {
+    interface Request {
+      recipeId?: string;
+    }
+    interface User {
+      _id: string;
+    }
+  }
+}
+
+export {};
