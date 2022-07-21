edeabaf657202f95b298ac7b196a8ed706b75f27
povilassta <povilas1999@gmail.com>
2022-07-21 16:48:17 +0300

    add interceptor

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,7 +7,11 @@ import { RecipeCatalogModule } from './recipe-catalog/recipe-catalog.module';
 import { LoginModule } from './login/login.module';
 import { RecipeViewModule } from './recipe-view/recipe-view.module';
 import { LayoutModule } from './layout/layout.module';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpClientModule,
+  HTTP_INTERCEPTORS,
+} from '@angular/common/http';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 import { CreateRecipeModule } from './create-recipe/create-recipe.module';
 import { NotFoundModule } from './not-found/not-found.module';
@@ -53,6 +57,13 @@ export function httpTranslateLoaderFactory(http: HttpClient) {
       },
       defaultLanguage: 'en',
     }),
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
   ],
   bootstrap: [AppComponent],
 })
